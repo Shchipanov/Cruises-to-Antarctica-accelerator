fbee8b22ab43b8b44d1de8e3ecc6415e073aec47
Shchipanov <chips_80@mail.ru>
2022-11-10 09:13:01 +0400

    делает шапку мобилки

---
+++ source/js/modules/get-menu.js
@@ -0,0 +1,17 @@
+let navMain = document.querySelector('[data-menu="main-nav"]');
+let navToggle = document.querySelector('[data-menu="btn"]');
+
+// navMain.classList.remove("main-nav--nojs");
+
+const getMenu = function () {
+  if (!navMain) {
+    return;
+  } else {
+    navToggle.addEventListener('click', function () {
+      navMain.classList.toggle('main-nav--opened');
+    });
+  }
+};
+
+export {getMenu};
+
