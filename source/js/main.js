b47f70ba5c6638e53cf4cbb2c45ef8ed8632a967
Shchipanov <chips_80@mail.ru>
2022-11-13 17:18:39 +0400

    делает форму

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {getMenu} from './modules/get-menu';
+import {getMask} from './modules/get-mask';
 
 // ---------------------------------
 
@@ -18,6 +19,7 @@ window.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('load', () => {
     initModals();
     getMenu();
+    getMask();
   });
 });
 
