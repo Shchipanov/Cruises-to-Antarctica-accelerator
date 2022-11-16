602aaf18a23ef3c0f60ee425cd348c7846916dd9
Shchipanov <chips_80@mail.ru>
2022-11-16 16:46:29 +0400

    осталсь два ьри больших замечания

--- source/js/modules/get-menu.js
+++ source/js/modules/get-menu.js
@@ -1,5 +1,7 @@
 let navMain = document.querySelector('[data-menu="main-nav"]');
 let navToggle = document.querySelector('[data-menu="btn"]');
+let headerTile = document.querySelector('[data-header-title]');
+
 
 navMain.classList.remove('main-nav--not-js');
 
@@ -7,8 +9,13 @@ const getMenu = function () {
   if (!navMain) {
     return;
   } else {
+    if (!navToggle) {
+      navMain.classList.add('main-nav--not-js');
+      return;
+    }
     navToggle.addEventListener('click', function () {
       navMain.classList.toggle('main-nav--opened');
+      headerTile.classList.toggle('main-header__title-wrapper--closed');
     });
   }
 };
